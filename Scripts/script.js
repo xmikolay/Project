84c8569553507e43ff78b6b6160dcb693be2ffb3
xmikolay <s00254903@atu.ie>
2024-12-05 18:36:38 +0000

    Got API's working

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    try {
-        await getMaps();
-        //await fetchUsers();
-        //await fetchSuperheroes();
+    try {   
+        const currentPage = window.location.pathname;
+        
+        if (currentPage.includes('players'))
+        {
+            await getPlayers();
+        }
+        else if (currentPage.includes('maps'))
+        {
+            await getMaps();
+        }
+        else if(currentPage.includes('tanks'))
+        {
+            await getTanks();
+        }
+        
+        
     } catch (error) {
         console.error('An error occurred during initialization:', error);
     }
@@ -12,14 +25,106 @@ const MAP_API = 'https://api.worldoftanks.eu/wot/encyclopedia/arenas/?applicatio
 
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
 app.appendChild(container);
+
+async function getPlayers() {
+    const playerName = document.getElementById('playerName').value.trim();
+
+    if (!playerName) {
+        alert('Please enter a player name.');
+        return;
+    }
+
+    const PLAYER_API = `https://api.worldoftanks.eu/wot/account/list/?application_id=ad0db27550136ff72d1b4d69ad6603ce&search=${playerName}`;
+    const container = document.getElementById('results');
+    container.innerHTML = ''; 
+
+    try {
+        const response = await fetch(PLAYER_API);
+        if (!response.ok) throw new Error(`HTTP getting Player Data: ${response.status}`);
+        const data = await response.json();
+
+        if (data.status == 'ok' && data.data.length > 0) {
+            data.data.forEach(player => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+
+                const playerName = document.createElement('h3');
+                playerName.textContent = `Player: ${player.nickname}`;
+
+                const playerAccountId = document.createElement('p');
+                playerAccountId.textContent = `Account ID: ${player.account_id}`;
+
+                card.appendChild(playerName);
+                card.appendChild(playerAccountId);
+
+                container.appendChild(card);
+            });
+        } else {
+            container.textContent = 'No players found.';
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+        container.textContent = `Error: ${error.message}`;
+    }
+}
+
+async function getTanks() {
+    const nation = document.getElementById('nation').value.trim();
+
+    if (!nation) {
+        alert('Please enter Nation.');
+        return;
+    }
+
+    const TANK_API = `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=ad0db27550136ff72d1b4d69ad6603ce&nation=${nation}`;
+    const container = document.getElementById('results');
+    container.innerHTML = ''; 
+
+    try {
+        const response = await fetch(TANK_API);
+        if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
+        const data = await response.json();
+
+        if (data && data.data) {
+            Object.values(data.data).forEach(nation => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+
+                const tankImg = document.createElement('img');
+                tankImg.src = nation.images.big_icon;   
+
+                const tankName = document.createElement('h3');
+                tankName.textContent = `Tank Name: ${nation.short_name}`;
+
+                const tankType = document.createElement('p');
+                tankType.textContent = `Tank type: ${nation.type}`;
+
+                const tankTier = document.createElement('p');
+                tankTier.textContent = `Tank tier: ${nation.tier}`;
+
+                const tankDesc = document.createElement('p');
+                tankDesc.textContent = `${nation.description}`;
+
+                card.appendChild(tankImg);
+                card.appendChild(tankName);
+                card.appendChild(tankType);
+                card.appendChild(tankTier);
+                card.appendChild(tankDesc);
+
+                container.appendChild(card);
+            });
+        } else {
+            container.textContent = 'No tanks found / Incorrect nation name.';
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+        container.textContent = `Error: ${error.message}`;
+    }
+}
 
 async function getMaps() {
     try{
@@ -48,12 +153,11 @@ async function getMaps() {
                 container.appendChild(card);
             });
         } else {
-            console.warn('Unexpected data structure:', data);
-            container.textContent = 'No maps found or incorrect data format.';
+            container.textContent = 'No maps found.';
         }
         
     } catch (error){
         console.error('Fetch error', error);
-        container.textContent = `Error: ${error.message || error}`;
+        container.textContent = `Error: ${error.message}`;
     }
 }
