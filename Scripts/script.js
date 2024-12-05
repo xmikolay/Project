42618629c6ba37695906d0f880a7fffb7e7873e6
xmikolay <s00254903@atu.ie>
2024-12-05 16:39:07 +0000

    Got a nice layout going for 1 api endpoint

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
     try {
-        await getSteamUsers();
+        await getMaps();
         //await fetchUsers();
         //await fetchSuperheroes();
     } catch (error) {
@@ -8,19 +8,48 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 });
 
-async function getSteamUsers() {
-    const container = document.getElementById('steam-users');
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+async function getMaps() {
     try{
-        const response = await fetch('https://www.valvesoftware.com/about/stats');
+        const response = await fetch('https://api.worldoftanks.eu/wot/encyclopedia/arenas/?application_id=ad0db27550136ff72d1b4d69ad6603ce');
         if (!response.ok) throw new error(`HTTP Error! Status: ${response.status}`);
         const data = await response.json();
 
-        data.array.forEach(element => {
-            const x = document.createElement('p');
-            x.textContent = element.users_online;
+        if (data && data.data) {
+            Object.values(data.data).forEach(element => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card')
+                
+                const mapName = document.createElement('h3');
+                mapName.textContent = `Name: ${element.name_i18n}`;
 
-            container.appendChild(p);
-        });
+                const mapDescription = document.createElement('p');
+                mapDescription.textContent = `Description: ${element.description}`;
+
+                const camoType = document.createElement('p');
+                camoType.textContent = `Camouflage: ${element.camouflage_type}`;
+
+                card.appendChild(mapName);
+                card.appendChild(mapDescription);
+                card.appendChild(camoType);
+
+                container.appendChild(card);
+            });
+        } else {
+            console.warn('Unexpected data structure:', data);
+            container.textContent = 'No maps found or incorrect data format.';
+        }
+        
     } catch (error){
         console.error('Fetch error', error);
         container.textContent = `Error: ${error.message || error}`;
